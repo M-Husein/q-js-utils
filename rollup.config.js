0b7e04fc87cb025763512555766e5425f103d868
Muhamad Husein <m.husein27@gmail.com>
2022-01-04 07:13:31 +0700

    Remove deprecated rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,5 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-// import resolve from "rollup-plugin-node-resolve";
-// import commonjs from "rollup-plugin-commonjs";
 import { babel } from '@rollup/plugin-babel';
-// import babel from "rollup-plugin-babel";
 import commonjs from '@rollup/plugin-commonjs';
 import pkg from "./package.json";
 
