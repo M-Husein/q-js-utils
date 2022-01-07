3827830b377d8fbc99661d1b666211384414f9a5
Muhamad Husein <m.husein27@gmail.com>
2022-01-07 23:07:04 +0700

    Update

--- Cx.js
+++
@@ -1,9 +0,0 @@
-import cls from 'classnames';
-
-/** classnames return undefined if length < 1 for prevent react render class="" */
-
-function Cx() {
-  return cls.apply(null, arguments) || undefined;
-}
-
-export { Cx as default };
--- cached.js
+++
@@ -1,9 +0,0 @@
-function cached(fn) {
-  var cache = Object.create(null);
-  return function cachedFn(s) {
-    var hit = cache[s];
-    return hit || (cache[s] = fn(s));
-  };
-}
-
-export { cached as default };
