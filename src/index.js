473a518bc95c2a4d50a3e003d11f072d5f2ec6b3
Muhamad Husein <m.husein27@gmail.com>
2022-01-15 10:02:00 +0700

    Tidy up index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
-// import cached from './cached';
-// import typeOf from './typeOf';
-// import darkOrLight from './darkOrLight';
-// import str2Hex from './str2Hex';
-// import getInitials from './getInitials';
-// import jsonParse from './jsonParse';
-// import objOmit from './objOmit';
-// import obj2FormData from './obj2FormData';
-// import isMobile from './isMobile';
-// import setClass from './setClass';
-// import setAttr from './setAttr';
-// // import Cx from './Cx';
-// import uid from './uid';
-
-// // export / export default
-// export {
-//   cached, 
-//   typeOf, 
-//   darkOrLight, 
-//   str2Hex, 
-//   getInitials,
-//   jsonParse,
-//   objOmit,
-//   obj2FormData,
-//   isMobile,
-//   setClass, 
-//   setAttr, 
-//   // Cx,
-//   uid, 
-// }
-
 export { default as cached } from './cached';
 export { default as typeOf } from './typeOf';
 export { default as darkOrLight } from './darkOrLight';
@@ -40,5 +9,4 @@ export { default as obj2FormData } from './obj2FormData';
 export { default as isMobile } from './isMobile';
 export { default as setClass } from './setClass';
 export { default as setAttr } from './setAttr';
-// import Cx from './Cx';
 export { default as uid } from './uid';
